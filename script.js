b8d767dcaadc3049520541f1aac7cffa583d5627
Motor Cycle <toomanymotorcycles06@gmail.com>
2026-02-04 12:32:56 +1300

    modified:   index.html new file:   init.js modified:   script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var messageSpace = getElementbyId("welcomeMessage");
-messageSpace.innerHTML = "You've connected to the JavaScript!";
+var title = document.getElementById("welcomeMessage");
+var errorBar = document.getElementById("errorBar");
+title.innerHTML = "Javascript connected.";
+
+function changebutton() {
+    title.innerHTML = String(document.getElementById("changeinput").value);
+    document.getElementById("header").innerHTML = String(document.getElementById("changeinput").value);
+    document.getElementById("changeinput").value = "";
+}
+
+function showError(errorinput) {
+    errorBar.setAttribute("hidden");
+}
